36a4a7475b5e6e34901016b744bf4dccea63c00e
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-04-27 10:38:03 -0700

    register function built

--- silent-auction/src/components/Register.js
+++ silent-auction/src/components/Register.js
@@ -1,29 +1,22 @@
-// REGISTER PAGE
 
-// { USEFORM } FROM 'REACT-HOOK-FORM'? (FAST + EASY VALIDATION!)
-
-// <FORM>
-    //REGISTER AS A BUYER OR A SELLER
-    // USERNAME 
-    // PASSWORD
-    // EMAIL
-    // 'SIGN UP' BUTTON
-// </FORM>
-
-// EXAMPLE OF VALIDATION WITH USEFORM: 
-// {errors.username && errors.username.type === "minLength" && (
-//    <p> This field requires a minimum length of 3 characters. </p> 
-// )}
-
-// EXAMPLE OF VALIDATION WITH USEFORM: 
-// {errors.username && errors.username.type === "required" && (
-//    <p> This field requires a minimum length of 3 characters. </p> 
-// )}
-
-
-import React from 'react'
+import React, { useState } from 'react'
+import axiosWithAuth from '../utils/axiosWithAuth'
 
 const register = () => {
+   const [ signup, setSignup] = useState({
+       username: " ", 
+       password: ""
+   })
+
+   const onSubmit = () => {
+       //   axios call
+       //   .then( res => {what is the response})
+       //   .catch()
+   }
+    
+   const changeHandler = e => {
+       setSignup({... signup, })
+   }
    
    
     return
