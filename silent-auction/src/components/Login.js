d64e132f2efed2467bb640dded6a2bb926935ca8
avpimblesr <allan.pimble@gmail.com>
2020-04-27 15:37:46 -0400

    Set up inputs

--- silent-auction/src/components/Login.js
+++ silent-auction/src/components/Login.js
@@ -3,11 +3,11 @@
 // { USEFORM } FROM 'REACT-HOOK-FORM'? (FAST + EASY VALIDATION!)
 
 // <FORM>
-    // LOGIN AS BUYER OR SELLER
-    // USERNAME 
-    // PASSWORD
-    //EMAIL
-    // 'LOG IN' BUTTON
+// LOGIN AS BUYER OR SELLER
+// USERNAME 
+// PASSWORD
+//EMAIL
+// 'LOG IN' BUTTON
 // </FORM>
 
 // EXAMPLE OF VALIDATION WITH USEFORM: 
@@ -22,15 +22,29 @@
 
 import React from 'react'
 
-const login = () => {
+const Login = () => {
 
     return
-    <form>
-        // LOGIN AS BUYER OR SELLER
-        <label>Username: &nbsp;</label>
-        <label>Password: &nbsp;</label>
-        <label>Email: &nbsp;</label>
-        <button>Log In</button>
-    </form>
+    (
+        <form >
+            // LOGIN AS BUYER OR SELLER
+            <input type="radio" id="buyer" name="client" value="buyer" />
+            <label for="buyer">Seller</label><br></br>
+            
+            <input type="radio" id="seller" name="client" value="seller" />
+            <label for="seller">Seller</label><br></br>
+
+            <label htmlFor='username'>Username: &nbsp;</label>
+            <input type='text' name='username' id='username' value='' />
+
+            <label htmlFor='password'>Password: &nbsp;</label>
+            <input type='password' name='' id='password' value='' />
+
+            <label htmlFor='email'>Email: &nbsp;</label>
+            <input type='text' name='email' id='email' value='' />
+
+            <button>Log In</button>
+        </form>
+    )
 }
 export default Login
