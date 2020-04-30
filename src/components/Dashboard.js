85ea9a3177ec3dd4e2a4a357b55cf068a4a91101
DESKTOP-J2DM1UE\Michael <michaelbarnes7282>
2020-04-30 14:19:51 -0700

    routing stuff

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import Login from './Login'
-import Register from './Register'
+import React, { useContext } from 'react'
+import {UserContext} from '../contexts/UserContext'
+import SellersDashboard from './SellersDashboard'
+import BuyersDashboard from './BuyersDashboard'
 
 
 const Dashboard = () => {
-
+    const user = useContext(UserContext)
     // Returns the specific Dashboard to the user 
     // depending on their role (bidder or seller)
-    return(
-        <div>
-        <Login/>
-        <Register/>
-        </div>
-    )
+    console.log(user)
+    if(user.currentUser.role === 'seller'){
+        return <SellersDashboard />
+    } else if (user.currentUser.role === 'bidder'){
+        return <BuyersDashboard />
+    }
+
 }
 export default Dashboard
