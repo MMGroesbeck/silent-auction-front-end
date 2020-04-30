b2e340678ce2afe2b11e1e9a78982814bfaa4a6e
avpimblesr <allan.pimble@gmail.com>
2020-04-30 19:58:57 -0400

    More formatting to Login file

--- silent-auction/src/components/Login.js
+++ silent-auction/src/components/Login.js
@@ -7,7 +7,7 @@ import './login.css'
 const Login = () => {
     // 'handleSubmit' will validate your inputs before invoking 'onSubmit'
     // Default data
-    const initialLogin = { username: '', password: '', email: '' }
+    const initialLogin = { name: '', password: '', email: '' }
 
     const [login, setLogin] = useState(initialLogin)
     const { register, handleSubmit, errors } = useForm()
@@ -26,9 +26,10 @@ const Login = () => {
 
     // handle changes in data on the form
     const handleChange = e => {
+        const { name, value } = e.target
         setLogin({
             ...login,
-            [e.target.name]: e.target.value
+            [name]: value
         });
     };
 
@@ -36,33 +37,22 @@ const Login = () => {
     return (
         // Custom Login form
         <form onSubmit={handleSubmit(onSubmit)}>
-            <p id='heading'>Login as Bidder or Seller</p>
-            {/* Bidder option */}
-            <input type="radio" id="buyer" name="client" value="buyer" />
-            <label htmlFor="buyer">Bidder</label>&nbsp;&nbsp;
 
-            {/* Seller option */}
-            <input type="radio" id="seller" name="client" value="seller" />
-            <label htmlFor="seller">Seller</label>
-
-            {/* Username */}<br /><br />
-            <span className='input-field'>
-            <label htmlFor='username'>User Name: &nbsp;</label>
+            {/* Name */}
+            <label htmlFor='name'>Name: &nbsp;</label>
             <input
                 type='text'
-                name='username'
+                name='name'
                 id='username'
-                value={login.username}
+                value={login.name}
                 onChange={handleChange}
                 // defaultValue="test" ref={register}  // register the username input
                 ref={register({ required: true })}  // include the 'required' option
             />
-            </span>
-            {/* Display username errors */}<br />
-            {errors.username && <p className="error-text">A user name is required.</p>}
+            {/* Display username errors */}
+            <p>{errors.name && <span className="error-text">A user name is required.</span>}&nbsp;</p>
 
-            {/* Password */}<br />
-            <span className='input-field'>
+            {/* Password */}
             <label htmlFor='password'>Password: &nbsp;</label>
             <input
                 type='password'
@@ -72,22 +62,21 @@ const Login = () => {
                 onChange={handleChange}
                 ref={register({ required: true })}
             />
-            </span>
-            {/* Display password errors */}<br />
-            {errors.password && <p className='error-text'>A password is required.</p>}
+            {/* Display password errors */}
+            <p>{errors.password && <span className='error-text'>A password is required.</span>}&nbsp;</p>
 
-            {/* Email address */}<br />
-            <span className='input-field'>
+            {/* Email address */}
             <label htmlFor='email'>Email: &nbsp;</label>
             <input
                 type='text'
                 name='email'
                 id='email'
+                value={login.email}
+                onChange={handleChange}
                 ref={register({ required: true })}
-            /> 
-            </span>
-            {/* Display email address errors */}<br />
-            {errors.email && <p className='error-text'>Please provide a valid email address.</p>}
+            />
+            {/* Display email address errors */}
+            <p>{errors.email && <span className='error-text'>Please provide a valid email address.</span>}&nbsp;</p>
 
             {/* Submit button */}<br /> <br />
             <button type='submit'>Log In</button>
@@ -96,27 +85,3 @@ const Login = () => {
 }
 
 export default Login
-
-
-// LOGIN PAGE
-
-// { USEFORM } FROM 'REACT-HOOK-FORM'? (FAST + EASY VALIDATION!)
-
-// <FORM>
-// LOGIN AS BUYER OR SELLER
-// USERNAME 
-// PASSWORD
-//EMAIL
-// 'LOG IN' BUTTON
-// </FORM>
-
-// EXAMPLE OF VALIDATION WITH USEFORM: 
-// {errors.username && errors.username.type === "minLength" && (
-//    <p> This field requires a minimum length of 3 characters. </p> 
-// )}
-
-// EXAMPLE OF VALIDATION WITH USEFORM: 
-// {errors.username && errors.username.type === "required" && (
-//    <p> This field requires a minimum length of 3 characters. </p> 
-// )}
-
